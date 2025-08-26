129ccf3f3354af2fc3f28eb40a88d82d5bca9874
venkataprasad-14 <venkataprasad1422@gmail.com>
2025-08-26 12:00:41 +0530

    java

---
+++ A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class A
+{
+	public static void main(String prasad[])
+	{
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("edho okati nokku vro");
+	  String s=sc.next();
+	  System.out.print(s);
+	}
+}
