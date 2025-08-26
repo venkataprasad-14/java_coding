2d4ff0b8651d589a40c2ee0bb8ae2ad1eaaab24f
venkataprasad-14 <venkataprasad1422@gmail.com>
2025-08-26 11:40:09 +0530

    first file

---
+++ switch1.java
@@ -0,0 +1,36 @@
+class switch1
+{
+   public static void main(String [] args)
+	{
+	 
+	  
+		int day=10;
+		switch (day)
+		{
+		    case 1:System.out.println("today is sunday");
+			        break;
+
+		   case 2:System.out.println("today is Monday");
+			 break;
+
+		   case 3: System.out.println("today is Tuesday");
+			 break;
+
+		   case 4: System.out.println("today is Wednesday");
+			 break;
+
+ 		   case 5: System.out.println("today is Thrusday");
+			 break;
+		   
+		   case 6: System.out.println("today is Friday");
+			 break;
+
+		   case 7:System.out.println("today is Saturday");
+			 break;
+		  default: System.out.println ("no_day");
+		}
+	
+	}
+
+
+}
